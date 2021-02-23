401620c0ff9bf2ed0430f32a7f4227e5f598f1ee
Ubuntu <ubuntu@ip-172-26-8-117.ca-central-1.compute.internal>
2021-02-23 01:23:00 +0000

    simplified apt install instruction

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ fi
 
 ### Install Docker ###
 ${SUDO} apt update
-${SUDO} apt install docker.io -y;${SUDO} apt install docker-compose -y
+${SUDO} apt install docker.io docker-compose -y
 
 ### TODO: check if user is in the docker group already. if not, user needs to logout and log back in for the next line to take effect.
 ${SUDO} usermod -aG docker $USER
