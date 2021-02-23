b553837cc79a9c92aa97aed95e201fcb44d662f6
cj <cj@iceoid.com>
2021-02-23 07:05:49 +0000

    working with multiple websites

--- install.sh
+++ install.sh
@@ -1,29 +1,50 @@
 #!/bin/bash
 
 ### VARS ###
-DOMAIN_NAME=mindlab.dev
-THISUSER=$USER
-SUDO=''
+DOMAIN_NAME=""
+SUDO=""
 
 if [[ $EUID -ne 0 ]]; then
    SUDO='sudo'
 fi
 
-### Install Docker ###
-${SUDO} apt update
-${SUDO} apt install docker.io docker-compose -y
+### General Functions ###
 
-### TODO: check if user is in the docker group already. if not, user needs to logout and log back in for the next line to take effect.
-${SUDO} usermod -aG docker $USER
+function get_domain_name() {
+   read -rp "Enter the domain name to be used:"$'\n' dname
+   DOMAIN_NAME=${dname}
+}
+
+function transfer_certs() {
+   mkdir ${PWD}/certs/${DOMAIN_NAME}
+   
+   ${SUDO} cp -p /etc/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem ${PWD}/certs/${DOMAIN_NAME}/
+   ${SUDO} chown $USER:$USER ${PWD}/certs/${DOMAIN_NAME}/fullchain.pem
+
+   ${SUDO} cp -p /etc/letsencrypt/live/${DOMAIN_NAME}/privkey.pem ${PWD}/certs/${DOMAIN_NAME}/
+   ${SUDO} chown $USER:$USER ${PWD}/certs/${DOMAIN_NAME}/privkey.pem
+}
 
 ### Files init ###
-${SUDO} cp -p /etc/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem ${PWD}/certs/
-${SUDO} chown $USER:$USER /etc/letsencrypt/live/${DOMAIN_NAME}/fullchain.pem ${PWD}/certs/
 
-${SUDO} cp -p /etc/letsencrypt/live/${DOMAIN_NAME}/privkey.pem ${PWD}/certs/
-${SUDO} chown $USER:$USER /etc/letsencrypt/live/${DOMAIN_NAME}/privkey.pem ${PWD}/certs/
+while [[ -z "${DOMAIN_NAME}" ]]; do
+   get_domain_name
+done
+
+transfer_certs
+
+ADD_DOMAIN=yes
+while [[ "${ADD_DOMAIN}" =~ ^([yY]|[yY][eE][sS])$ ]] ; do
+   read -rp "Would you like to add certs for other domains? [y/N] " ADD_DOMAIN
+   if [[ "${ADD_DOMAIN}" =~ ^([yY]|[yY][eE][sS])$ ]]; then
+      get_domain_name
+      transfer_certs
+   else
+      break
+   fi
+done
 
 ### Run nginx reverse proxy container
-docker netword create docker_net
+docker network create docker_net
 #docker-compose up -d
 
